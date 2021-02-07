387d16923ea6b098ae037ecdbe2b8a75f4da8095
Aliaksandra Neviarouskaya <aliaksandra.neviarouskaya@aiolane.com>
2021-02-07 22:18:12 +0300

    Change Greetings message

--- src/main/java/aneviaro/Message.java
+++ src/main/java/aneviaro/Message.java
@@ -4,7 +4,7 @@ enum Messages {
 
     BestTimeToWakeUp("The best time to wake up is:\n"), BestTimeToGoToSleep(
             "The best time to go to sleep is:\n"), Greetings(
-            "Greeting, please type in the time in the next format: I.e. *22:15*"), ClarificationQuestion(
+            "Greeting my dear sleep friend. Let's be friends! Please, type in the time in the next format: I.e. *22:15*. You'll be asked to choose if it's a *Wake up* or *Go To Sleep* time. Then I will provide the best time for you to go to sleep or wake up. Let's start!"), ClarificationQuestion(
             "Is it wake up time or go to sleep time?");
 
     private final String message;
